f5c581ccdf8fae331ca3e9549b802f210738c1c2
dtanakab <karugamo.highkick@gmail.com>
2019-11-28 14:34:01 +0900

    Use prepared statement

--- app_memo.rb
+++ app_memo.rb
@@ -12,19 +12,19 @@ class Memo
     end
 
     def write(text)
-      @conn.exec("INSERT INTO memo VALUES (nextval('id'), '#{text}')")
+      @conn.exec("INSERT INTO memo VALUES (nextval('id'), $1)", [text])
     end
 
     def update(id, text)
-      @conn.exec("UPDATE memo SET memo = '#{text}' WHERE id = '#{id}'")
+      @conn.exec('UPDATE memo SET memo = $1 WHERE id = $2', [text, id])
     end
 
     def read(id)
-      @conn.exec("SELECT memo FROM memo WHERE id = #{id}")[0]['memo']
+      @conn.exec('SELECT memo FROM memo WHERE id = $1', [id])[0]['memo']
     end
 
     def delete(id)
-      @conn.exec("DELETE FROM Memo WHERE id = #{id}")
+      @conn.exec('DELETE FROM Memo WHERE id = $1', [id])
     end
   end
 end
